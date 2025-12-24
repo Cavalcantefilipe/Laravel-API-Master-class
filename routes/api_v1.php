454ffccb17d8cbba549fd9a5b9e7f0bb3f904de0
Filipe <filipe.alvescavalcante@gmail.com>
2025-12-24 18:50:14 -0300

    fix: author and tickets

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\V1\AuthorController;
+use App\Http\Controllers\Api\V1\AuthorTicketsController;
 use App\Http\Controllers\Api\V1\TicketController;
 use App\Http\Controllers\Api\V1\UserController;
 use Illuminate\Http\Request;
@@ -9,5 +11,6 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('v1')->group(function () {
 
     Route::middleware('auth:sanctum')->apiResource('tickets', TicketController::class);
-    Route::middleware('auth:sanctum')->apiResource('users', UserController::class);
+    Route::middleware('auth:sanctum')->apiResource('authors', AuthorController::class);
+    Route::middleware('auth:sanctum')->apiResource('authors.tickets', AuthorTicketsController::class);
 });
